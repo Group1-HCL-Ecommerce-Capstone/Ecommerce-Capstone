35daaab62c6525a05cd0253f40ff36dae3ca30b5
adamhcl <113631551+adamhcl@users.noreply.github.com>
2022-12-13 11:59:52 -0600

    Update CartController.java

--- Back-End/src/main/java/com/capstone/controller/CartController.java
+++ Back-End/src/main/java/com/capstone/controller/CartController.java
@@ -32,7 +32,7 @@ import com.capstone.service.UserService;
 
 @RestController
 @RequestMapping("/cart")
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = "https://ecommerce-capstone-fe.azurewebsites.net/")
 public class CartController {
 	@Autowired
 	CartService cartServ;
